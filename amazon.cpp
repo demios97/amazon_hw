87748587b7e1f80bc02cad4d245dd70cf5eb2779
demios97 <53512334+demios97@users.noreply.github.com>
2023-06-22 13:33:09 +0300

    Add files via upload

--- amazon.cpp
+++ amazon.cpp
@@ -3,6 +3,21 @@
 #include <cstring>
 #include "amazon.hpp"
 
+std::string enumToString(Category cat) {
+    switch(cat){
+        case Category::ELECTRONICS:
+            return "ELECTRONICS";
+        case Category::CLOTHING:
+            return "CLOTHING";
+        case Category::BOOKS:
+            return "BOOKS";
+        case Category::BEAUTY:
+         	return "BEAUTY";
+        default:
+            return "Unknown";
+    }
+}
+
 Product::Product(const char *name, double price, Category category){
     if(!name || price<0 || category>Category::BEAUTY || category<Category::ELECTRONICS) return;
     this->name = new char[strlen(name)+1];
@@ -53,7 +68,6 @@ void Product::setCategory(Category category){
 }
 
 ShoppingCart::ShoppingCart(){
-    this->products = new Product[MAX_PRODUCTS];
     this->numProducts = 0;
 }
 
@@ -61,10 +75,57 @@ ShoppingCart::~ShoppingCart(){
     delete[] products;
 }
 
-void OnlineShoppingSystem::addUser(User *user){
-    if(!user)return;
+void ShoppingCart::addProduct(Product *product){
+    if(!product || numProducts==MAX_PRODUCTS) return;
+    products[numProducts]->setName(product->getName());
+    products[numProducts]->setPrice(product->getPrice());
+    products[numProducts]->setCategory(product->getCategory());
+    numProducts++;
+}
 
-    numUsers++;
+void ShoppingCart::removeProduct(Product *product){
+    if(!product) return;
+    int count = numProducts;
+    while(count>0){
+        if(strcmp(products[count]->getName(), product->getName())==0){
+            products[count]->~Product();
+            for (int i = count; i < numProducts - 1; ++i)
+                products[i] = products[i + 1];
+        }
+        count--;
+    }
+}
+
+double ShoppingCart::calculateTotalPrice() const{
+    int count = numProducts;
+    double total = 0;
+    while(count>0){
+        total += products[count]->getPrice();
+        count--;
+    }
+    return total;
+}
+
+void ShoppingCart::displayCart() const{
+    int count = 0;
+    std::cout << "Cart:" << std::endl;
+    while(count != numProducts+1){
+        std::cout << count << ". Name: " << products[count]->getName();
+        std::cout << " Price: " << products[count]->getPrice();
+        std::cout << " Category: " << enumToString(products[count]->getCategory()) << std::endl;
+        count++;
+    }
+}
+
+User::User(const char *name){
+    if(!name) return;
+    this->name = new char[strlen(name)+1];
+    if(!this->name){
+        delete[] this->name;
+        return;
+    }
+    strcpy(this->name, name);
+    
 }
 
 
@@ -84,7 +145,7 @@ User* user = new User("John");
 // Add the user to the system
 shoppingSystem.addUser(user);
 // Simulate online shopping
-shoppingSystem.simulateOnlineShopping();
+//shoppingSystem.simulateOnlineShopping();
 // Display the products and users in the system
 shoppingSystem.displayProducts();
 shoppingSystem.displayUsers();
