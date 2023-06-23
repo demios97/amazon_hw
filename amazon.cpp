64c8dce6a21a3f1318bd24c7334b73d5995aecc3
demios97 <53512334+demios97@users.noreply.github.com>
2023-06-23 03:00:35 +0300

    compiles and run, but there is some questions for Youval

--- amazon.cpp
+++ amazon.cpp
@@ -19,15 +19,9 @@ std::string enumToString(Category cat) {
 }
 
 Product::Product(const char *name, double price, Category category){
-    if(!name || price<0 || category>Category::BEAUTY || category<Category::ELECTRONICS) return;
-    this->name = new char[strlen(name)+1];
-    if(!this->name){
-        delete[] this->name;
-        return;
-    }
-    strcpy(this->name, name);
-    this->price = price;
-    this->category = category;
+    setName(name);
+    setPrice(price);
+    setCategory(category);
 }
 
 Product::~Product(){
@@ -76,45 +70,49 @@ ShoppingCart::~ShoppingCart(){
 }
 
 void ShoppingCart::addProduct(Product *product){
-    if(!product || numProducts==MAX_PRODUCTS) return;
-    products[numProducts]->setName(product->getName());
-    products[numProducts]->setPrice(product->getPrice());
-    products[numProducts]->setCategory(product->getCategory());
-    numProducts++;
+    if(!product || numProducts==MAX_PRODUCTS){
+        std::cout << "Product cannot be added to cart" << std::endl;
+        return;
+    }
+    products[numProducts++] = product;
+    std::cout << "Product added to cart successfully" << std::endl;
 }
 
 void ShoppingCart::removeProduct(Product *product){
-    if(!product) return;
-    int count = numProducts;
-    while(count>0){
+    if(!product || numProducts==0){
+        std::cout << "Product cannot be removed from cart" << std::endl;
+        return;
+    }
+    int count = numProducts-1;
+    while(count>=0){
         if(strcmp(products[count]->getName(), product->getName())==0){
-            products[count]->~Product();
-            for (int i = count; i < numProducts - 1; ++i)
+            for (int i = count; i < numProducts-1; i++)
                 products[i] = products[i + 1];
+            products[--numProducts] = nullptr;
         }
         count--;
     }
+    std::cout << "Product removed from cart successfully" << std::endl;
 }
 
 double ShoppingCart::calculateTotalPrice() const{
-    int count = numProducts;
+    int count = numProducts-1;
     double total = 0;
-    while(count>0){
-        total += products[count]->getPrice();
-        count--;
-    }
+    while(count>=0)
+        total += products[count--]->getPrice();
     return total;
 }
 
 void ShoppingCart::displayCart() const{
     int count = 0;
     std::cout << "Cart:" << std::endl;
-    while(count != numProducts+1){
-        std::cout << count << ". Name: " << products[count]->getName();
+    while(count != numProducts){
+        std::cout << count+1 << ". Name: " << products[count]->getName();
         std::cout << " Price: " << products[count]->getPrice();
         std::cout << " Category: " << enumToString(products[count]->getCategory()) << std::endl;
         count++;
     }
+    std::cout << "Total price: " << calculateTotalPrice() << std::endl;
 }
 
 User::User(const char *name){
@@ -130,7 +128,9 @@ const char* User::getName()const{
 }
 
 void User::setName(const char *name){
-    if(!name) return;
+    if(!name){
+        return;
+    }
     this->name = new char[strlen(name)+1];
     if(!this->name){
         delete[] this->name;
@@ -149,55 +149,95 @@ OnlineShoppingSystem::OnlineShoppingSystem(){
 }
 
 OnlineShoppingSystem::~OnlineShoppingSystem(){
-    delete[] products;//TODO do we need? because he already deletes product in main
+    delete[] products;
+    delete[] users;
 }
 
 void OnlineShoppingSystem::addProduct(Product* product){
-    if(!product || numProducts==MAX_PRODUCTS) return;
-    products[numProducts]->setName(product->getName());
-    products[numProducts]->setPrice(product->getPrice());
-    products[numProducts]->setCategory(product->getCategory());
-    ShoppingCart cart = users[numUsers]->getShoppingCart();
-    cart.addProduct(product);
-    numProducts++;
+    if(!product || numProducts==MAX_PRODUCTS) {
+        std::cout << "Product cannot be added" << std::endl;
+        return;
+    }
+    for(int i = 0; i < numProducts; i++)
+        if(strcmp(products[i]->getName(), product->getName())==0){
+            std::cout << "Product already exist" << std::endl;
+            return;
+        }
+    products[numProducts++] = product;
+    std::cout << "Product added successfully" << std::endl;
 }
 
 void OnlineShoppingSystem::removeProduct(Product *product){
-    if(!product) 
+    if(!product || numProducts==0){
+        std::cout << "Product cannot be removed" << std::endl;
         return;
-    int count = numProducts;
-    while(count > 0){
-        if(strcmp(product->getName(),products[count]->getName())==0){
-            products[count]->setName("");
-            products[count]->setPrice(0.0);
-//TODO didn't find an optimal way to remove from a static allocation of an array so I've initialized the element that we remove 
-//and subtracted the number of products, maybe need to initialize category because it is currently uninitialized. 
-            numProducts--;
-            return;
+    }
+    int count = numProducts-1;
+    while(count>=0){
+        if(strcmp(products[count]->getName(), product->getName())==0){
+            for (int i = count; i < numProducts-1; i++)
+                products[i] = products[i + 1];
+            products[--numProducts] = nullptr;
         }
         count--;
     }
-
-
+    count = numUsers-1;
+    while(count>=0)
+        users[count--]->getShoppingCart().removeProduct(product);
+    std::cout << "Product removed successfully" << std::endl;
 }
 
 void OnlineShoppingSystem::addUser(User *user){
-    if(!user)
+    if(!user || numUsers==MAX_USERS){
+        std::cout << "User cannot be added" << std::endl;
         return;
-    int count = numUsers;
-
-    //checking if user already exist.
-    while(count > 0){
-        if(strcmp(users[count]->getName(),user->getName())==0){
+    }
+    for(int i = 0; i < numUsers; i++)
+        if(strcmp(users[i]->getName(), user->getName())==0){
+            std::cout << "User already exist" << std::endl;
             return;
+        }
+    users[numUsers++] = user;
+    std::cout << "User added successfully" << std::endl;
+
+}
+
+void OnlineShoppingSystem::removeUser(User *user){
+    if(!user || numUsers==0){
+        std::cout << "User cannot be removed" << std::endl;
+        return;
+    }
+    int count = numUsers-1;
+    while(count>=0){
+        if(strcmp(users[count]->getName(), user->getName())==0){
+            for (int i = count; i < numUsers-1; i++)
+                users[i] = users[i + 1];
+            users[--numUsers] = nullptr;
         }
         count--;
     }
-
-    users[numUsers]->setName(user->getName());
+    std::cout << "User removed successfully" << std::endl;
 }
 
+void OnlineShoppingSystem::displayProducts() const{
+    int count = 0;
+    std::cout << "Amazon Products:" << std::endl;
+    while(count != numProducts){
+        std::cout << count+1 << ". Name: " << products[count]->getName();
+        std::cout << " Price: " << products[count]->getPrice();
+        std::cout << " Category: " << enumToString(products[count]->getCategory()) << std::endl;
+        count++;
+    }
+}
 
+void OnlineShoppingSystem::displayUsers() const{
+    int count = 0;
+    std::cout << "Amazon Users:" << std::endl;
+    while(count != numUsers){
+        std::cout << count+1 << ". Name: " << users[count]->getName() << std::endl;
+        count++;
+    }
+}
 
 int main() {
 // Create an instance of the OnlineShoppingSystem
@@ -211,11 +251,27 @@ shoppingSystem.addProduct(product1);
 shoppingSystem.addProduct(product2);
 shoppingSystem.addProduct(product3);
 // Create a user
-User* user = new User("John");
+User* user1 = new User("John1");
+User* user2 = new User("John2");
+User* user3 = new User("John3");
+User* user4 = new User("John4");
 // Add the user to the system
-shoppingSystem.addUser(user);
+shoppingSystem.addUser(user1);
+shoppingSystem.addUser(user2);
+shoppingSystem.addUser(user3);
+shoppingSystem.addUser(user4);
 // Simulate online shopping
-//shoppingSystem.simulateOnlineShopping();
+user1->getShoppingCart().addProduct(product1);
+user1->getShoppingCart().addProduct(product2);
+user1->getShoppingCart().addProduct(product3);
+user2->getShoppingCart().addProduct(product3);
+user2->getShoppingCart().addProduct(product3);
+user2->getShoppingCart().addProduct(product3);
+user1->getShoppingCart().displayCart();
+user2->getShoppingCart().displayCart();
+shoppingSystem.removeProduct(product3);
+shoppingSystem.removeUser(user1);
+user2->getShoppingCart().displayCart();
 // Display the products and users in the system
 shoppingSystem.displayProducts();
 shoppingSystem.displayUsers();
@@ -223,6 +279,9 @@ shoppingSystem.displayUsers();
 delete product1;
 delete product2;
 delete product3;
-delete user;
+delete user1;
+delete user2;
+delete user3;
+delete user4;
 return 0;
 }
